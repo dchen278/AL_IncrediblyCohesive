bfc48604938c47f08ce0cddf27db88cfd21bc02b
dchen278 <reindeer914@gmail.com>
2021-12-11 11:28:47 -0500

    implement OAL

--- ALTester.java
+++ ALTester.java
@@ -2,24 +2,23 @@ import java.util.ArrayList;
 
 public class ALTester {
     static ArrayList<Integer> stillDavid = new ArrayList<Integer>();
+
     public static void main(String[] args) {
-        ALTester demo = new ALTester();
-        for (int i = 0; i < 24; i++) {
-            // populate stillDavid with 
-            stillDavid.add(i);
+        ALTester tester = new ALTester();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < 23; i++) {
+            list.add(i);
         }
-        stillDavid.add(1);
-        System.out.println(demo.sorted());
+        System.out.println(tester.test(list));
     }
 
-    public boolean sorted() {
-        for (int i = 0; i < stillDavid.size() - 1; i++) {
-            if (stillDavid.get(i) > stillDavid.get(i + 1)) {
+    public boolean test(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i).compareTo(list.get(i + 1)) > 0) {
                 return false;
             }
         }
         return true;
     }
-
 
 }
--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,5 +1,42 @@
+import java.util.ArrayList;
+
 public class OrderedArrayList {
-    public boolean add(int num) {
+    private ArrayList<Integer> _list;
+
+    public OrderedArrayList() {
+        _list = new ArrayList<Integer>();
+    }
+
+    public String toString() {
+        return _list.toString();
+    }
+
+    public Integer get(int index) {
+        return _list.get(index);
+    }
+
+    public boolean add(Integer value) {
+        int i = 0;
+        while (i < _list.size()) {
+            if (value.compareTo(_list.get(i)) < 0) {
+                _list.add(i, value);
+                return true;
+            }
+        }
+        _list.add(value);
+        return true;
+    }
+
+    public Integer remove(int index) {
+        return _list.remove(index);
+    }
+
+    public int size() {
+        return _list.size();
+    }
+
+    public static void main(String[] args) {
+        OrderedArrayList list = new OrderedArrayList();
         
     }
 }
